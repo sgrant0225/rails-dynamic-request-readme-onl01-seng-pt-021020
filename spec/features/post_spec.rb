004a919e90a1f2260401252e5a266e8be482fe9e
Sharnell Grant <sharnellgrant@gmail.com>
2020-06-18 00:28:29 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -9,5 +9,10 @@ describe 'navigate' do
    visit "/posts/#{@post.id}"
     expect(page.status_code).to eq(200)
   end 
+  
+  it 'shows the title on the show page in an h1 tag' do
+    visit "posts/#{@post.id}"
+      expect(page).to have_css("h1", text: "My Post")
+    end
 end
     
